c680d2df17f83453ec5b104c3888314bf4fd763e
Muhammad Usman Farooq <30377499+usmanmunara@users.noreply.github.com>
2018-07-05 06:53:27 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyB3Uj3gA2r3xlwMYj-csJioOQopL0fPH8Y',
-  authDomain: 'first-flight-787fd.firebaseapp.com',
-  databaseURL: 'https://first-flight-787fd.firebaseio.com',
-  projectId: 'first-flight-787fd',
-  storageBucket: 'first-flight-787fd.appspot.com',
-  messagingSenderId: '381130432711'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 };
 firebase.initializeApp(config);
 
